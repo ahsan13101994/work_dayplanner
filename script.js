7f1db885ccd3dc373e5aa52aea887668af524ebe
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-05-28 12:50:35 -0400

    time slot background color functionality

--- script.js
+++ script.js
@@ -26,16 +26,48 @@ $(document).ready(function() {
 
   // Color Change depending on time // - dont know how to make it red once the field is filled
 
-  var m = moment();
-  $.each($(".time-block"), function (index, value) {
-      let dateHour = $(value).attr("data-time");
-      if (Number(dateHour) === m.hour()) {
-          $(this).find("textarea").addClass('present');
-      } else if (Number(dateHour) < m.hour()) {
-          $(this).find("textarea").addClass('past');
-      } else {
-          $(this).find("textarea").addClass('future');
-      }
-  });
+    function timeSlotColor() {
+      const hours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+      let currentHr = parseInt(moment().hour());
+      let ele = document.getElementsByClassName('description');
+      hours.map((hour, i) => {
+        let atts = ele[i].getAttribute('class')
+        ele[i].setAttribute('class', `${atts} future`);
+        ele[i].setAttribute('data-event', `eventNum${i}`);
 
-  // Color Change on the slot being taken// 
+        if ((currentHr >= 0 && currentHr < 5) || currentHr >= 18) {
+          ele[i].setAttribute('class', `${atts} past`);
+        } else if (currentHr >= 7 && currentHr < 9) {
+          ele[i].setAttribute('class', `${atts} future`);
+        } else if ((currentHr > 12) & (currentHr <= 17)) {
+          // FOR PM
+
+        if (hour <= 5) {
+            hour += 12;
+          }
+        if (hour == currentHr) {
+            console.log('present time');
+            ele[i].setAttribute('class', `${atts} present`);
+        } else if (hour < currentHr) {
+            ele[i].setAttribute('class', `${atts} past`);
+        } else if (hour > currentHr) {
+            ele[i].setAttribute('class', `${atts} future`);
+        }
+        } else if ((currentHr <= 12) & (currentHr >= 9)) {
+         
+        
+        if (hour <= 5) {
+            hour += 12;
+          }
+        if (hour == currentHr) {
+            ele[i].setAttribute('class', `${atts} present`);
+        } else if (hour > currentHr) {
+            ele[i].setAttribute('class', `${atts} future`);
+          } else if (hour < currentHr) {
+            ele[i].setAttribute('class', `${atts} past`);
+          }
+        } else if ((currentHr >= 0) & (currentHr < 6)) {
+          }
+      })
+    }
+    timeSlotColor();
