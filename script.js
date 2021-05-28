c920b73a44ef18da09acc3544e36f73cdf712ad1
Syed Ahsan <ahsan.ali0307@gmail.com>
2021-05-28 01:08:32 -0400

    saving enteries

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Current Time //
 let currentday = document.getElementById('currentDay')
     console.log(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-    currentday.innerText = moment().format("dddd, MMMM Do YYYY");
+    currentday.innerText = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+// Local Storage //
+
+$(document).ready(function() {
+  $(".saveBtn").on("click", function() {
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, value);
+  });
+
+  $("#9am .description").val(localStorage.getItem("9am"));
+  $("#10am .description").val(localStorage.getItem("10am"));
+  $("#11am .description").val(localStorage.getItem("11am"));
+  $("#12pm .description").val(localStorage.getItem("12pm"));
+  $("#1pm .description").val(localStorage.getItem("1pm"));
+  $("#2pm .description").val(localStorage.getItem("2pm"));
+  $("#3pm .description").val(localStorage.getItem("3pm"));
+  $("#4pm .description").val(localStorage.getItem("4pm"));
+  $("#5pm .description").val(localStorage.getItem("5pm"));
+
+  });
